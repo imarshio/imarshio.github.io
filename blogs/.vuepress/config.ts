edc9a83450ffb61106c4b6ebf97ff5056e99c454
marshio <marshioman@gmail.com>
2024-01-10 18:55:48 +0800

    complete my blog's structure

--- blogs/.vuepress/config.ts
+++ blogs/.vuepress/config.ts
@@ -1,11 +1,29 @@
 import { defineUserConfig } from 'vuepress'
 import { defaultTheme } from 'vuepress'
 
-export default defineUserConfig({
+export default {
   lang: 'en-US',
   title: 'marshio',
   description: 'marshio.',
-  public: '${sourceDir}/.vuepress/assets',
+  head: [
+    ['link', { rel: 'icon', href: '/public/images/horse.ico' }]
+  ],
+  // 语言选项
+  // locales: {
+  //   // 键名是该语言所属的子路径
+  //   // 作为特例，默认语言可以使用 '/' 作为其路径。
+  //   '/': {
+  //     lang: 'en-US',
+  //     title: 'VuePress',
+  //     description: 'Vue-powered Static Site Generator',
+  //   },
+  //   '/zh/': {
+  //     lang: 'zh-CN',
+  //     title: 'VuePress',
+  //     description: 'Vue 驱动的静态网站生成器',
+  //   },
+  // },
+  // public: './.vuepress/assets',
   theme: defaultTheme({
     logo: '/assets/image/horse.ico',
     // navgation
@@ -18,11 +36,11 @@ export default defineUserConfig({
       { text: 'Me', link: '/me' }
     ],
     sidebarDepth: 3,
-    sidebar: [
+    // sidebar: [
 
-    ]
+    // ]
   })
-})
+}
 
 // module.exports = {
 //   title: 'marshio',
---
+++ blogs/projects.md
@@ -0,0 +1,17 @@
+---
+lang: en-US
+title: projects
+description: marshio's projects show
+---
+
+## Springboot Demo
+
+一些有关Springbooot的演示案例及其相关扩展，纯个人观点。
+
+### [Java Log Framwork](https://github.com/imarshio/springboot-demo/tree/main/demo-log)
+
+[Log](./projects/springboot-demo/log//log.md)
+
+### [Springboot Annotation](https://github.com/imarshio/springboot-demo/tree/main/demo-annotation)
+
+[Transaction](./projects/springboot-demo/annotation/transaction.md)
